ba39425ab5b6203215ef7fa522eb716f90c50c2e
jh <oluashegun@googlemail.com>
2020-10-07 10:21:44 +0100

    slider

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+ get '/resume', to: 'pages#resume', as: :resume
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
